4dc2eb0112fa1d3905031ced44967614450fa8b5
Lik Eduard <feft99@gmail.com>
2021-06-18 23:08:38 +0600

    rewrite score_scene

---
+++ lib/scenes/score_scene/score_scene.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quizapp/controllers/game_controller.dart';
+
+class ScoreScene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Score Scene...")));
+  }
+}

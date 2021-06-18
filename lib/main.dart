148a8e4f24ef875bdb6dd61e0ce45572e9f9aa1b
Lik Eduard <feft99@gmail.com>
2021-06-18 23:03:36 +0600

    entry point wrapped in GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'game_scene.dart';
-import 'game_controller.dart';
+import 'scenes/game_scene/game_scene.dart';
+import 'controllers/game_controller.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -10,14 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.teal,
-          accentColor: Colors.cyan,
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white,
           fontFamily: 'Montserrat',
         ),
-        home: GameScene());
+        home: GameScene(name: "Вопросы для студентов"));
   }
 }
